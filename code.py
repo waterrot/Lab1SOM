62f2142f694a56d7f68bb04165a75ba32cdc62ec
waterrot <oudloosdrecht@gmail.com>
2023-11-22 11:50:05 +0000

    make api work

--- code.py
+++ code.py
@@ -1,1 +1,16 @@
-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
+import os
+from openai import OpenAI
+
+
+client = OpenAI(api_key="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")
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
+
